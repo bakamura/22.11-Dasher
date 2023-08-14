402b49327e1329d58cf4a8734013875a74b87a76
Pedro Nakamura <ra00297288@pucsp.edu.br>
2023-08-13 23:41:33 -0300

    New Haptic Solution
    (To be tested, finishing polishing before new build)

--- Assets/Game/Code/Script/Disembodied/HapticHandler.cs
+++ Assets/Game/Code/Script/Disembodied/HapticHandler.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 public class HapticHandler : Singleton<HapticHandler> {
 
 #if UNITY_ANDROID
-        private int _hapticFeedbackConstantsKey;
-        private AndroidJavaObject _unityPlayer;
+    //private int _hapticFeedbackConstantsKey;
+    private AndroidJavaObject _unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); // Needs to be public static ?
+    private AndroidJavaObject _currentActivity;
+    private AndroidJavaObject _vibrator;
+    private float _vibrationTime = 200; // in Milliseconds
 #endif
 
     protected override void Awake() {
         base.Awake();
 
-#if UNITY_ANDROID
-        _hapticFeedbackConstantsKey = new AndroidJavaClass("android.view.HapticFeedbackConstants").GetStatic<int>("VIRTUAL_KEY");
-        _unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer");
-#endif  
+        if (IsAndroid()) {
+            //_hapticFeedbackConstantsKey = new AndroidJavaClass("android.view.HapticFeedbackConstants").GetStatic<int>("VIRTUAL_KEY");
+            //_unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer");
+
+            //_unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            _currentActivity = _unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            _vibrator = _currentActivity.Call<AndroidJavaObject>("getSystemService", "Vibrator");
+        }
     }
 
     private void Start() {
@@ -21,10 +28,21 @@ public class HapticHandler : Singleton<HapticHandler> {
     }
 
     private void HapticFeedback(Vector2 v2) {
-#if UNITY_ANDROID
-        //Handheld.Vibrate();
-        _unityPlayer.Call<bool>("performHapticFeedback", _hapticFeedbackConstantsKey);
-#endif
+        if (IsAndroid()) {
+            //_unityPlayer.Call<bool>("performHapticFeedback", _hapticFeedbackConstantsKey);
+
+            _vibrator.Call("vibrate", _vibrationTime);
+        }
+        else {
+            Handheld.Vibrate();
+        }
     }
 
+    private bool IsAndroid() {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        return true;
+#else
+        return false;
+#endif
+    }
 }
