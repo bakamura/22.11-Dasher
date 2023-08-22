fd4d54297e2e2d22cc2e5420280e50175565ce75
NakaVostro <ra00297288@pucsp.edu.br>
2023-08-22 14:13:32 -0300

    Auto-Adjust Level Selector container

--- Assets/Game/Code/Script/UI/LevelEnterButtonSpawner.cs
+++ Assets/Game/Code/Script/UI/LevelEnterButtonSpawner.cs
@@ -8,17 +8,24 @@ public class LevelEnterButtonSpawner : MonoBehaviour {
     [Header("Parameters")]
 
     [SerializeField] private GameObject _levelEnterBtnPrefab;
-    [SerializeField] private Transform _btnParent;
+    [SerializeField] private RectTransform _btnParent;
 
     public Button[] InstantiateButons() {
         Button[] btn = new Button[SceneManager.sceneCountInBuildSettings - 1];
+        GridLayoutGroup gridLayout = _btnParent.GetComponent<GridLayoutGroup>();
+        Vector2 sizeParent = _btnParent.sizeDelta;
         for (int i = 0; i < SceneManager.sceneCountInBuildSettings - 1; i++) {
             int sceneId = i + 1;
             btn[i] = Instantiate(_levelEnterBtnPrefab, _btnParent).GetComponent<Button>();
             btn[i].onClick.AddListener(() => LevelManager.instance.GoToScene(sceneId)); // Because it's not possible to change parameters in an already present action
             btn[i].GetComponentInChildren<TextMeshProUGUI>().text = (sceneId).ToString();
-            if(i > 0) btn[i].interactable = SaveSystem.instance.progress.levelCleared[i - 1]; // 
+            if (i > 0) btn[i].interactable = SaveSystem.instance.progress.levelCleared[i - 1]; // 
+
+            else sizeParent[1] = 0;
+            if (i % 3 == 0) sizeParent[1] += gridLayout.cellSize.y + gridLayout.spacing.y;
         }
+
+        _btnParent.sizeDelta = sizeParent;
 
         return btn;
     }
