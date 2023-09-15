eb94ee4a7d7947e13d1d1ff7de6ddefe851d38de
Pedro Nakamura <ra00297288@pucsp.edu.br>
2023-09-15 14:16:38 -0300

    Small Ammend
    Try solving issue later

--- Assets/Game/Code/Script/Disembodied/HapticHandler.cs
+++ Assets/Game/Code/Script/Disembodied/HapticHandler.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 
 public class HapticHandler : Singleton<HapticHandler> {
 
-#if UNITY_ANDROID
-    private AndroidJavaObject _unityPlayer; // Needs to be public static ?
-    private AndroidJavaObject _currentActivity;
-    private AndroidJavaObject _vibrator;
-#endif
+//#if UNITY_ANDROID && !UNITY_EDITOR
+//    private AndroidJavaObject _unityPlayer; // Needs to be public static ?
+//    private AndroidJavaObject _currentActivity;
+//    private AndroidJavaObject _vibrator;
+//#endif
     // private float _vibrationTime = 200; // in Milliseconds
 
     protected override void Awake() {
         base.Awake();
 
-#if UNITY_ANDROID && !UNITY_EDITOR
-            Debug.Log("Playing in Android Phone");
-            _unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            _currentActivity = _unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-            _vibrator = _currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
-        }
-#endif
+//#if UNITY_ANDROID && !UNITY_EDITOR
+//            Debug.Log("Playing in Android Phone");
+//            _unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+//            _currentActivity = _unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+//            _vibrator = _currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+//        }
+//#endif
     }
 
     private void Start() {
@@ -28,9 +28,9 @@ public class HapticHandler : Singleton<HapticHandler> {
     private void HapticFeedback(Vector2 v2) {
         Debug.Log("Vibrated");
         Handheld.Vibrate();
-#if UNITY_ANDROID && !UNITY_EDITOR
-        _vibrator.Call("vibrate", _vibrationTime);
-#endif
+//#if UNITY_ANDROID && !UNITY_EDITOR
+//        _vibrator.Call("vibrate", _vibrationTime);
+//#endif
     }
 
 }
