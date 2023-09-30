e670af048cbd884ee255ab6664427194de15c4fd
bakamura <ra00297288@pucsp.edu.br>
2023-09-30 16:15:21 -0300

    Level Tweaks, HoleTeleport Simplify

--- Assets/Game/Code/Script/LevelMechanic/HoleTeleport.cs
+++ Assets/Game/Code/Script/LevelMechanic/HoleTeleport.cs
@@ -3,31 +3,33 @@ using UnityEngine.Events;
 
 public class HoleTeleport : MonoBehaviour {
 
-    [Header("Debug")]
-
-    [SerializeField] private float _angleAdded; // Remove Later
-
     [HideInInspector] public static UnityEvent onTeleport = new UnityEvent();
 
     [Header("Properties")]
 
+    [Tooltip("Changing in runtime can cause unexpected results")]
+    [SerializeField] private float _acceptedAngle;
     [SerializeField] private HoleTeleport _holeConnected;
     [HideInInspector] public bool canTeleport = true;
 
-    [Header("Cache")]
+    //[Header("Cache")]
 
     //private Vector2 _v2C;
-    private float _sinC;
-    private float _cosC;
+    //private float _sinC;
+    //private float _cosC;
 
-    private void Start() {
-        float holeConnectedAngle = (_angleAdded + transform.eulerAngles.z - _holeConnected.transform.eulerAngles.z) * Mathf.Deg2Rad;
-        _sinC = Mathf.Sin(holeConnectedAngle);
-        _cosC = Mathf.Cos(holeConnectedAngle);
+    //private void Start() {
+    //    float holeConnectedAngle = (_angleAdded + transform.eulerAngles.z - _holeConnected.transform.eulerAngles.z) * Mathf.Deg2Rad;
+    //    _sinC = Mathf.Sin(holeConnectedAngle);
+    //    _cosC = Mathf.Cos(holeConnectedAngle);
+    //}
+
+    private void Awake() {
+        _acceptedAngle = 180f - _acceptedAngle / 2f;
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (canTeleport && collision == PlayerDash.instance.col) Teleport();
+        if (canTeleport && collision == PlayerDash.instance.col && Vector2.Angle(PlayerDash.instance.rb.velocity.normalized, (Vector2) transform.up) > _acceptedAngle) Teleport();
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
@@ -42,8 +44,7 @@ public class HoleTeleport : MonoBehaviour {
         PlayerDash.instance.transform.position = _holeConnected.transform.position;
 
         // Angle
-        PlayerDash.instance.rb.velocity = new Vector2(PlayerDash.instance.rb.velocity.x * _cosC - PlayerDash.instance.rb.velocity.y * _sinC,
-        PlayerDash.instance.rb.velocity.x * _sinC + PlayerDash.instance.rb.velocity.y * _cosC);
+        PlayerDash.instance.rb.velocity = _holeConnected.transform.up * PlayerDash.instance.rb.velocity.magnitude;
     }
 
     //private void Teleport() {
